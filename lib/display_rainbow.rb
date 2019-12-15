e31e3903dc391291e1ec917a26ed6e2520786361
Erick Espinoza <erickespinoza.dev@icloud.com>
2019-12-15 03:55:23 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
-# Write your #display_rainbow method here
+def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  puts "r"
+  puts "o"
+  puts "y"
+  puts "g"
+  puts "b"
+  puts "i"
+  puts "v"
+end
